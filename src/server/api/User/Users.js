a9aa7214d4c453ff3c793a97b7985e054f944ca6
Mfernandez1255 <maurice.fernandez1255@gmail.com>
2023-12-08 15:11:38 -0500

    updated the seed file for another user

--- src/server/api/User/Users.js
+++ src/server/api/User/Users.js
@@ -1,6 +1,6 @@
 const { ServerError } = require("../../errors");
 const prisma = require("../../prisma");
-const ticket = require("");
+const ticket = require("./Tickets");
 
 const router = require("express").Router();
 module.exports = router;
@@ -22,97 +22,115 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const id = +req.params.id;
-    const user = await prisma.user.findUnique({ where: { id } });
+// router.get("/:id", async (req, res, next) => {
+//   try {
+//     const id = +req.params.id;
+//     const user = await prisma.user.findUnique({ where: { id } });
 
-    res.json(user);
-  } catch (err) {
-    next(err);
-  }
-});
+//     res.json(user);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
-router.post("/buy-ticket/:ticketId", async (req, res) => {
-  const { ticketId } = req.params;
-  const { buyerId } = req.locals.user.id; // Assuming you're sending the buyer's user ID in the request body
+// // router.use(async (req, res, next) => {
+// //   try {
+// //     const { sellerId } = ticket.req.params.sellerId;
+// //     // const { buyerId } = ticket.req.params.buyerId
 
-  try {
-    // Find the ticket by ID
-    const ticket = await prisma.ticket.findUnique({
-      where: {
-        id: parseInt(ticketId),
-      },
-    });
+// //     const userSellerId = await prisma.ticket.findUnique({
+// //       where: { sellerId },
+// //     });
 
-    if (!ticket) {
-      return res.status(404).json({ error: "Ticket not found" });
-    }
+// //     next();
+// //   } catch (err) {
+// //     console.error(err);
+// //   }
+// // });
 
-    // Assuming 'userId' is the field in your 'Ticket' model that represents the ticket owner's ID
-    if (ticket.userId === buyerId) {
-      return res.status(400).json({ error: "You cannot buy your own ticket" });
-    }
+// router.put("/sold-tickets", async (req, res) => {
+//   try {
+//   } catch (err) {
+//     console.error(err);
+//   }
+// });
 
-    // Update the ticket's owner ID to the buyer's ID
-    const updatedTicket = await prisma.ticket.update({
-      where: {
-        id: parseInt(ticketId),
-      },
-      data: {
-        userId: buyerId,
-      },
-    });
+// router.get("/bought-tickets", async (req, res) => {
+//   // Assuming you're sending the buyer's user ID in the request body
+//   //   try{
+//   // }
+//   //   try {
+//   //     // Find the ticket by ID
+//   //     const ticket = await prisma.ticket.findUnique({
+//   //       where: {
+//   //         id: parseInt(ticketId),
+//   //       },
+//   //     });
+//   //     if (!ticket) {
+//   //       return res.status(404).json({ error: "Ticket not found" });
+//   //     }
+//   //     // Assuming 'userId' is the field in your 'Ticket' model that represents the ticket owner's ID
+//   //     if (ticket.userId === buyerId) {
+//   //       return res.status(400).json({ error: "You cannot buy your own ticket" });
+//   //     }
+//   //     // Update the ticket's owner ID to the buyer's ID
+//   //     const updatedTicket = await prisma.ticket.update({
+//   //       where: {
+//   //         id: parseInt(ticketId),
+//   //       },
+//   //       data: {
+//   //         userId: buyerId,
+//   //       },
+//   //     });
+//   //     return res.status(200).json(updatedTicket);
+//   //   } catch (error) {
+//   //     return res.status(500).json({ error: "Internal Server Error" });
+//   //   }
+// });
 
-    return res.status(200).json(updatedTicket);
-  } catch (error) {
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+// router.post("/sell-ticket", async (req, res) => {
+//   const {
+//     eventName,
+//     dateTime,
+//     description,
+//     location,
+//     seatSection,
+//     imageUrl,
+//     price,
+//   } = req.body;
+//   const sellerId = res.locals.user.id;
 
-router.post("/sell-ticket", async (req, res) => {
-  const {
-    eventName,
-    dateTime,
-    description,
-    location,
-    seatSection,
-    imageUrl,
-    price,
-  } = req.body;
-  const sellerId = res.locals.user.id;
+//   try {
+//     // Create a new ticket and associate it with the user (assuming 'userId' is provided in the request body)
+//     const newTicket = await prisma.ticket.create({
+//       data: {
+//         eventName,
+//         dateTime,
+//         description,
+//         location,
+//         seatSection,
+//         imageUrl,
+//         price,
+//         userId: sellerId, // Associate the ticket with the user selling it
+//       },
+//     });
 
-  try {
-    // Create a new ticket and associate it with the user (assuming 'userId' is provided in the request body)
-    const newTicket = await prisma.ticket.create({
-      data: {
-        eventName,
-        dateTime,
-        description,
-        location,
-        seatSection,
-        imageUrl,
-        price,
-        userId: sellerId, // Associate the ticket with the user selling it
-      },
-    });
+//     return res.status(201).json(newTicket);
+//   } catch (error) {
+//     return res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
 
-    return res.status(201).json(newTicket);
-  } catch (error) {
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+// router.delete("/:id", async (req, res, next) => {
+//   try {
+//     const id = +req.params.id;
 
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const id = +req.params.id;
+//     const ticket = await prisma.ticket.findUnique({ where: { id } });
+//     validateTicket(res.locals.user, ticket, next);
 
-    const ticket = await prisma.ticket.findUnique({ where: { id } });
-    validateTicket(res.locals.user, ticket, next);
-
-    await prisma.ticket.delete({ where: { id } });
-    res.sendStatus(204);
-  } catch (err) {
-    next(err);
-  }
-});
+//     await prisma.ticket.delete({ where: { id } });
+//     res.sendStatus(204);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
